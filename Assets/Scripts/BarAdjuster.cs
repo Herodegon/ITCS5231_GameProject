28d2ccbaa5e81554a0be63f8821445a7f5dd8e08
brainrot42-nil <samcniles@gmail.com>
2026-04-08 18:03:00 -0400

    adjusted scaler
    tested scaler on different device to make sure things were working as intended and adjusted slightly

--- Assets/Scripts/BarAdjuster.cs
+++ Assets/Scripts/BarAdjuster.cs
@@ -13,8 +13,8 @@ public class Bar : MonoBehaviour
 
     void Start()
     {
-        healthUI.transform.Translate(215.0f, 75.0f, 0.0f);
-        fuelUI.transform.Translate(215.0f, 130.0f, 0.0f);
+        healthUI.transform.Translate(150.0f, 50.0f, 0.0f);
+        fuelUI.transform.Translate(150.0f, 90.0f, 0.0f);
     }
 
     public void setMaxBar(int value, bool health)
